1c81e7f973930c0cf344b51ffe294d56f26f3424
Omar Mahmud <omar1450@nga.cs.utexas.edu>
2016-03-21 14:32:16 -0500

    I added facebook login functionality to our app.

---
+++ SnapMap/Bridging-header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-header.h
+//  SnapMap
+//
+//  Created by Omar Mahmud on 3/21/16.
+//  Copyright © 2016 cs378. All rights reserved.
+//
+
+#ifndef Bridging_header_h
+#define Bridging_header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Bridging_header_h */
