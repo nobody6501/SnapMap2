2f4baa195ccfa6250d3acd3c372aa841479f0882
Cody Hill <cody.hill@utexas.edu>
2016-03-22 16:40:10 -0500

    Clean up Title image on Login page.

--- SnapMap/LoginViewController.swift
+++ SnapMap/LoginViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
 
-    @IBOutlet weak var SnapMapTitle: UILabel!
+    @IBOutlet weak var SnapMapTitle: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        SnapMapTitle.backgroundColor = UIColor(patternImage: UIImage(named: "SnapMapTitle.jpg")!)
+        SnapMapTitle.image = UIImage(named: "SnapMapTitle.jpg")!
+//        SnapMapTitle.backgroundColor = UIColor(patternImage: UIImage(named: "SnapMapTitle.jpg")!)
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "LoginBackground.jpg")!)
         
         if (FBSDKAccessToken.currentAccessToken() != nil)
