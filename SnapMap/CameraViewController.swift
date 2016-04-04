680f81d959a76367e8be48cb59cfbb67930d1d75
Omar Mahmud <omar1450@nga.cs.utexas.edu>
2016-04-04 14:13:45 -0500

    I have now tested the camera and it works, I also added a confirmation screen.

--- SnapMap/CameraViewController.swift
+++ SnapMap/CameraViewController.swift
@@ -10,62 +10,50 @@
 import UIKit
 import AVFoundation
 
-class CameraViewController: UIViewController {
-
-    let captureSession = AVCaptureSession()
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
-    var captureDevice : AVCaptureDevice?
+    var imagePicker: UIImagePickerController!
+    var photo: UIImage? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        captureSession.sessionPreset = AVCaptureSessionPresetLow
         
-        let devices = AVCaptureDevice.devices()
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
         
-        for device in devices {
-            if(device.hasMediaType(AVMediaTypeVideo)){
-                if(device.position == AVCaptureDevicePosition.Back){
-                    captureDevice = device as? AVCaptureDevice
-                }
-            }
-        }
-        if captureDevice != nil{
-            beginSession()
-        }
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+
+        
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func beginSession(){
-        var err: NSError? = nil
-        do{
-          try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
-        }
-        catch is NSError {
-            print("error")
-        }
-        
-        var previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        self.view.layer.addSublayer(previewLayer)
-        previewLayer?.frame = self.view.layer.frame
-        captureSession.startRunning()
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        photo = info[UIImagePickerControllerOriginalImage] as? UIImage
+        performSegueWithIdentifier("confirmationsegue", sender: self)
     }
     
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if(segue.identifier == "confirmationsegue"){
+            let dvc = segue.destinationViewController as! PhotoConfirmationViewController
+            dvc.photo = self.photo
+            
+        }
     }
-    */
+
 
 }
