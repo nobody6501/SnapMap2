eeba65f19fa8052aa47510ffbd055df848ee3f29
Martin Huang <martinhuang@Martins-MacBook-Pro.local>
2016-04-06 16:56:58 -0500

    added firebase for user ID

--- SnapMap/LoginViewController.swift
+++ SnapMap/LoginViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
-
+    
+    let ROOT = Firebase(url:"https://intense-inferno-7933.firebaseio.com/")
+    var uid = ""
+    
     @IBOutlet weak var SnapMapTitle: UIImageView!
     var clients = [NSManagedObject]()
     var identifier: String? = nil
@@ -161,6 +165,22 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
             settings.id = self.identifier!
             map.id = self.identifier!
             print("identifier in segue = \(self.identifier!)")
+        }
+    }
+    
+    //get user's facebook UID and store in firebase
+    func fbCurrentUserID() {
+        let userRoot = ROOT!.childByAppendingPath("users")
+        var fbRequest = FBSDKGraphRequest(graphPath:"/me/", parameters: nil);
+        fbRequest.startWithCompletionHandler { (connection : FBSDKGraphRequestConnection!, result : AnyObject!, error : NSError!) -> Void in
+            if error == nil {
+                self.uid = result.valueForKey("id") as! String
+                let currentUser = userRoot.childByAppendingPath(self.uid)
+                currentUser.setValue(self.uid)
+                
+            } else {
+                print("Error Getting Friends \(error)");
+            }
         }
     }
     
